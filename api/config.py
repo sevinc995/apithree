a6a167c097fc38138ece9a6cc601fa29dedfba0e
sevinc995 <sevincseferova25@gmail.com>
2025-04-14 17:25:57 +0000

    project

---
+++ api/config.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+
+if not API_KEY:
+    raise ValueError("API_KEY not found in the env file")
---
+++ api/models.py
@@ -0,0 +1,13 @@
+class News:
+
+    def __init__(self, tit, des, img):
+
+        self.tit = tit
+        self.des = des
+        self.img = img 
+
+news1 = News(
+    "iPhone 16 Pro Max",
+    "The iPhone 16 Pro Max will feature a larger 6.9-inch display, powered by the A18 Pro chip with enhanced AI capabilities, and will include a new dedicated Capture button for improved camera functionality.",
+    ""
+)
