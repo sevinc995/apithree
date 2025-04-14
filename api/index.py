7c3c1876cd57ba6cfb3704ef5a6680bb55e95606
sevinc995 <sevincseferova25@gmail.com>
2025-04-14 18:19:30 +0000

    cors

---
+++ api/index.py
@@ -0,0 +1,9 @@
+from flask import Flask 
+from flask_cors import CORS 
+
+app = Flask(__name__)
+
+CORS(app, resources={r"/api/news":{
+    "origins": [""],
+    "methods": ["GET"]
+}})
--- api/models.py
+++ api/models.py
@@ -9,5 +9,19 @@ class News:
 news1 = News(
     "iPhone 16 Pro Max",
     "The iPhone 16 Pro Max will feature a larger 6.9-inch display, powered by the A18 Pro chip with enhanced AI capabilities, and will include a new dedicated Capture button for improved camera functionality.",
-    ""
+    "https://raw.githubusercontent.com/sevinc995/imagess/refs/heads/main/iphone.webp"
 )
+
+news2 = News(
+    "AirPods Pro 3",
+    "The AirPods Pro 3 are expected to feature new adaptive audio technology, improved noise cancellation, and potentialyy offer new color options, They are still in early development stages.",
+    "https://github.com/sevinc995/imagess/blob/main/airpods.jpeg?raw=true"
+)
+
+news3 = News(
+    "Apple Watch Ultra 3",
+    "The Apple Watch Ultra 3 will likely maintain  the same rugged design as its prodecessor but may include new health features like blood pressure monitoring and slepp apnea detection, alongside minor hardware upgrades.",
+    "https://github.com/sevinc995/imagess/blob/main/watch.jpeg?raw=true"
+)
+
+news_list = [news1, news2, news3]
