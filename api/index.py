08e63b4b46d8095562d8357025ddf7542cc8ad24
sevinc995 <sevincseferova25@gmail.com>
2025-04-18 09:27:58 +0000

    routes

--- api/index.py
+++ api/index.py
@@ -1,5 +1,6 @@
 from flask import Flask 
 from flask_cors import CORS 
+from .routes import get_news
 
 app = Flask(__name__)
 
@@ -7,3 +8,7 @@ CORS(app, resources={r"/api/news":{
     "origins": ["https://sevinc995.github.io"],
     "methods": ["GET"]
 }})
+
+@app.route("/api/news", methods=["GET"])
+def news_route():
+    return get_news()
